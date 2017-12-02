c432c031f17df8d689a754b6b8b14378f4fc38be
dvdfu <davidf1212@gmail.com>
2017-12-02 00:01:30 -0800

    click and drag

--- src/Pet.lua
+++ src/Pet.lua
@@ -8,15 +8,46 @@ local sprite = love.graphics.newImage('assets/pet/amanita.png')
 
 function Pet:init(x, y)
     self.pos = Vector(x, y)
+    self.offset = Vector(8, 8)
     self.anim = Animation(sprite, 2, 10)
+    self.radius = 8
+
+    self.faceRight = true
+    self.selected = false
 end
 
 function Pet:update(dt)
-    self.anim:update(dt)
+    local animSpeed = self.selected and 2 or 1
+    self.anim:update(dt * animSpeed)
+end
+
+function Pet:contains(x, y)
+    local delta = Vector(x, y) - self.pos
+    return delta:len2() < self.radius * self.radius
+end
+
+function Pet:isSelected()
+    return self.selected
+end
+
+function Pet:select()
+    self.selected = true
+end
+
+function Pet:unselect()
+    self.selected = false
+end
+
+function Pet:move(x, y)
+    if x > self.pos.x + 2 then self.faceRight = true end
+    if x < self.pos.x - 2 then self.faceRight = false end
+    self.pos.x = x
+    self.pos.y = y
 end
 
 function Pet:draw()
-    self.anim:draw(self.pos:unpack())
+    local xScale = self.faceRight and 1 or -1
+    self.anim:draw(self.pos.x, self.pos.y, 0, xScale, 1, self.offset.x, self.offset.y)
 end
 
 return Pet
--- src/states/Game.lua
+++ src/states/Game.lua
@@ -7,6 +7,7 @@ end
 
 function Game:enter()
     self.pets = {}
+    self.selectedPet = nil
     table.insert(self.pets, Pet(50, 50))
 end
 
@@ -17,12 +18,26 @@ function Game:update(dt)
 end
 
 function Game:mousepressed(x, y)
+    for _, pet in pairs(self.pets) do
+        if pet:contains(x, y) then
+            pet:select()
+            self.selectedPet = pet
+            return
+        end
+    end
 end
 
 function Game:mousereleased(x, y)
+    if not self.selectedPet then return end
+
+    self.selectedPet:unselect()
+    self.selectedPet = nil
 end
 
 function Game:mousemoved(x, y)
+    if not self.selectedPet then return end
+
+    self.selectedPet:move(x, y)
 end
 
 function Game:draw()
