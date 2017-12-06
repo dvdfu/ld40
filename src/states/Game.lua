cf627a8974e0b3f37cc6c38828ec0ddbd08054e8
dvdfu <davidf1212@gmail.com>
2017-12-06 00:02:18 -0800

    game refactor

--- src/states/Game.lua
+++ src/states/Game.lua
@@ -19,6 +19,11 @@ local Game = {}
 local NEXT_PET_TIME = 180
 local NEXT_PET_TIME_MAX = 540
 
+local function help()
+    local Instructions = require 'src.states.Instructions'
+    Gamestate.push(Instructions)
+end
+
 function Game:init()
     local font = love.graphics.newFont('res/font/redalert.ttf', 13)
     love.graphics.setFont(font)
@@ -27,6 +32,8 @@ function Game:init()
 end
 
 function Game:enter()
+    self.gameOver = false
+    self.selection = nil
     self.lives = 5
     self.money = 10
     self.pets = 0
@@ -40,7 +47,8 @@ function Game:enter()
     self.overlayPos = 1
     self.overlayTimer = Timer()
     self.overlayTimer:tween(30, self, {overlayPos = 0}, 'in-cubic')
-    self.gameOver = false
+    self.nextPetDelay = NEXT_PET_TIME
+    self.nextPetTimer = Timer()
 
     self.appleParticles = Particles.newApple()
     self.dustParticles = Particles.newDust()
@@ -84,43 +92,37 @@ function Game:enter()
 
     AppleCrate(self.container, Constants.GAME_WIDTH / 2, Constants.GAME_HEIGHT - 48)
 
-    self.selection = nil
-
-    self.nextPetTimer = 0
-    self.nextPetTimerMax = NEXT_PET_TIME
-
-    self:help()
+    help()
+    self:spawnPet()
 end
 
 function Game:update(dt)
     if self.selection then
         self.selection:drag(mousePosition:unpack())
     end
+    self.container:update(dt)
     self.appleParticles:update(dt)
     self.dustParticles:update(dt)
-    self.container:update(dt)
     self.moneyOffsetTimer:update(dt)
+    self.nextPetTimer:update(dt)
     self.overlayTimer:update(dt)
     self.stats.time = self.stats.time + 1 / 60
+end
 
-    if self.nextPetTimer > 1 then
-        self.nextPetTimer = self.nextPetTimer - 1
-    else
-        local x = math.random(32, Constants.GAME_WIDTH - 32)
-        local y = math.random(32, Constants.GAME_HEIGHT - 32)
-        Egg(self.container, x, y)
-        self.dustParticles:setPosition(x, y)
-        self.dustParticles:emit(2)
-        self.pets = self.pets + 1
-        self.stats.totalPets = self.stats.totalPets + 1
+function Game:spawnPet()
+    local x = math.random(32, Constants.GAME_WIDTH - 32)
+    local y = math.random(32, Constants.GAME_HEIGHT - 32)
+    Egg(self.container, x, y)
+    self.dustParticles:setPosition(x, y)
+    self.dustParticles:emit(2)
+    self.pets = self.pets + 1
+    self.stats.totalPets = self.stats.totalPets + 1
 
-        if self.nextPetTimerMax < NEXT_PET_TIME_MAX then
-            self.nextPetTimerMax = self.nextPetTimerMax + 60
-        else
-            self.nextPetTimerMax = NEXT_PET_TIME_MAX
-        end
-        self.nextPetTimer = self.nextPetTimerMax
+    self.nextPetDelay = self.nextPetDelay + 60
+    if self.nextPetDelay > NEXT_PET_TIME_MAX then
+        self.nextPetDelay = NEXT_PET_TIME_MAX
     end
+    self.nextPetTimer:after(self.nextPetDelay, function() self:spawnPet() end)
 end
 
 function Game:onLoseLife()
@@ -148,34 +150,27 @@ function Game:onPayout(amount)
 end
 
 function Game:buyLife()
-    if self.money >= 50 then
-        self.money = self.money - 50
-        self.lives = self.lives + 1
-    end
+    if self.money < 50 then return end
+    self.money = self.money - 50
+    self.lives = self.lives + 1
 end
 
 function Game:buyApple(crate)
-    if self.money >= 5 then
-        crate:onClick()
-        self.money = self.money - 5
-        self.moneyOffset = -4
-        self.moneyOffsetTimer:clear()
-        self.moneyOffsetTimer:tween(10, self, {moneyOffset = 0}, 'out-quad')
-        if self.selection then self.selection:unselect() end
-        local x, y = crate:getPosition():unpack()
-        x = x + math.random() - 0.5
-        y = y - 12
-        local apple = Apple(self.container, x, y)
-        apple:select()
-        self.selection = apple
-        self.dustParticles:setPosition(x, y)
-        self.dustParticles:emit(2)
-    end
-end
-
-function Game:help()
-    local Instructions = require 'src.states.Instructions'
-    Gamestate.push(Instructions)
+    if self.money < 5 then return end
+    crate:onClick()
+    self.money = self.money - 5
+    self.moneyOffset = -4
+    self.moneyOffsetTimer:clear()
+    self.moneyOffsetTimer:tween(10, self, {moneyOffset = 0}, 'out-quad')
+    if self.selection then self.selection:unselect() end
+    local x, y = crate:getPosition():unpack()
+    x = x + math.random() - 0.5
+    y = y - 12
+    local apple = Apple(self.container, x, y)
+    apple:select()
+    self.selection = apple
+    self.dustParticles:setPosition(x, y)
+    self.dustParticles:emit(2)
 end
 
 function Game:mousepressed(x, y)
@@ -199,7 +194,7 @@ function Game:mousereleased(x, y)
     end
 end
 
-function Game:outlinedText(text, x, y)
+local function outlinedText(text, x, y)
     love.graphics.setColor(0, 0, 0)
     for i = -1, 1, 2 do
         love.graphics.print(text, x + i, y)
@@ -220,17 +215,17 @@ function Game:draw()
     local x = 16
     love.graphics.draw(Sprites.ui.HEART, x, 3)
     if self.lives <= 1 then
-        self:outlinedText(self.lives, x + 15, 2 + math.sin(self.stats.time * 30))
+        outlinedText(self.lives, x + 15, 2 + math.sin(self.stats.time * 30))
         love.graphics.setColor(215, 83, 21)
         love.graphics.print(self.lives, x + 15, 2 + math.sin(self.stats.time * 30))
         love.graphics.setColor(255, 255, 255)
     else
-        self:outlinedText(self.lives, x + 15, 2)
+        outlinedText(self.lives, x + 15, 2)
     end
 
     x = x + 32
     love.graphics.draw(Sprites.ui.COIN, x, 3 - self.moneyOffset)
-    self:outlinedText(self.money, x + 15, 2 - self.moneyOffset)
+    outlinedText(self.money, x + 15, 2 - self.moneyOffset)
 
     if self.overlayPos > 0 then
         love.graphics.setColor(0, 0, 0)
